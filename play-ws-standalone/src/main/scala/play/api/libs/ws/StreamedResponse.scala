df274b196df51322cd85f9acf4fa40c35dd77535
Will Sargent <wsargent@users.noreply.github.com>
2016-12-21 13:34:23 -0800

    Remove CollectionUtil (#15)

--- play-ws-standalone/src/main/scala/play/api/libs/ws/StreamedResponse.scala
+++ play-ws-standalone/src/main/scala/play/api/libs/ws/StreamedResponse.scala
@@ -3,19 +3,32 @@
  */
 package play.api.libs.ws
 
-import akka.stream.javadsl
-import akka.stream.scaladsl
+import akka.stream.{javadsl, scaladsl}
 import akka.util.ByteString
+
+import java.{util => ju}
 
 /**
  * A streamed response containing a response header and a streamable body.
  */
 case class StreamedResponse(headers: WSResponseHeaders, body: scaladsl.Source[ByteString, _])
-    extends play.libs.ws.StreamedResponse {
+  extends play.libs.ws.StreamedResponse {
+
 
   override def getHeaders: play.libs.ws.WSResponseHeaders = {
-    new play.libs.ws.DefaultWSResponseHeaders(headers.status, CollectionUtil.convert(headers.headers))
+    new play.libs.ws.DefaultWSResponseHeaders(headers.status, convert(headers.headers))
   }
+
+  /**
+   * Utility class for converting a Scala `Map` with a nested collection type into its idiomatic Java counterpart.
+   * The reason why this source is written in Scala is that doing the conversion using Java is a lot more involved.
+   * This utility class is used by `play.libs.ws.StreamedResponse`.
+   */
+  private def convert(headers: Map[String, Seq[String]]): ju.Map[String, ju.List[String]] = {
+    import scala.collection.convert.WrapAsJava.{mapAsJavaMap, seqAsJavaList}
+    mapAsJavaMap(headers.map { case (k, v) => k -> seqAsJavaList(v) })
+  }
+
   override def getBody: javadsl.Source[ByteString, _] = body.asJava
 
 }
