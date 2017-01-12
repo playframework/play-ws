0bc433d66cf1877d05e79f5b24c261da612b1796
Will Sargent <will.sargent@lightbend.com>
2017-01-12 12:37:47 -0800

    Add in the parser

--- play-ahc-ws-standalone/src/it/scala/playwsclient/ScalaIntegrationSpec.scala
+++ play-ahc-ws-standalone/src/it/scala/playwsclient/ScalaIntegrationSpec.scala
@@ -6,9 +6,11 @@ package playwsclient
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable.Specification
-import play.api.libs.ws.ahc.{ AhcConfigBuilder, StandaloneAhcWSClient }
+import play.api.libs.ws.WSConfigParser
+import play.api.libs.ws.ahc.{ AhcConfigBuilder, AhcWSClientConfigParser, StandaloneAhcWSClient }
 import play.shaded.ahc.org.asynchttpclient.DefaultAsyncHttpClient
 
 import scala.concurrent.duration._
@@ -18,11 +20,26 @@ class ScalaIntegrationSpec(implicit ee: ExecutionEnv) extends Specification {
   "the client" should {
 
     "call out to a remote system and get a correct status" in {
+
+      // Create a config from the application.conf file
+      val config = ConfigFactory.load
+      val classLoader = this.getClass.getClassLoader
+      val wsClientConfig = new WSConfigParser(config, classLoader).parse
+      val ahcWSClientConfig = new AhcWSClientConfigParser(wsClientConfig, config, classLoader).parse
+
+      // Map the WS config to the AHC config class.
+      val builder = new AhcConfigBuilder(ahcWSClientConfig)
+      val asyncHttpClientConfigBuilder = builder.configure()
+      val asyncHttpClientConfig = asyncHttpClientConfigBuilder.build()
+
+      // Create the AHC client
+      val ahcClient = new DefaultAsyncHttpClient(asyncHttpClientConfig)
+
+      // Create Akka system for thread and streaming management
       implicit val system = ActorSystem()
       implicit val materializer = ActorMaterializer()
 
-      val builder = new AhcConfigBuilder()
-      val ahcClient = new DefaultAsyncHttpClient(builder.configure().build())
+      // Create the standalone WS client
       val wsClient = new StandaloneAhcWSClient(ahcClient)
 
       wsClient.url("http://www.google.com").get().map { response ⇒
--- play-ws-standalone/src/main/scala/play/api/libs/ws/WSConfigParser.scala
+++ play-ws-standalone/src/main/scala/play/api/libs/ws/WSConfigParser.scala
@@ -2,7 +2,7 @@ package play.api.libs.ws
 
 import javax.inject.{ Inject, Provider, Singleton }
 
-import com.typesafe.config.Config
+import com.typesafe.config.{ Config, ConfigException }
 import com.typesafe.sslconfig.ssl.SSLConfigParser
 import com.typesafe.sslconfig.util.EnrichedConfig
 
@@ -30,7 +30,14 @@ class WSConfigParser @Inject() (config: Config, classLoader: ClassLoader) extend
     val followRedirects = wsConfig.getBoolean("followRedirects")
     val useProxyProperties = wsConfig.getBoolean("useProxyProperties")
 
-    val userAgent = Option(wsConfig.getString("useragent"))
+    val userAgent = {
+      try {
+        Some(wsConfig.getString("useragent"))
+      } catch {
+        case e: ConfigException.Null =>
+          None
+      }
+    }
 
     val compressionEnabled = wsConfig.getBoolean("compressionEnabled")
 
