86da25b6e61c66763c3e0d0c7bf24ae5b41c3d5b
Marcos Pereira <marcos.silva@gmail.com>
2017-04-17 11:11:26 -0300

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,16 +13,16 @@ object Dependencies {
   ).map("org.specs2" %% _ % specsVersion)
 
   // Use the published milestone
-  val playJsonVersion = "2.6.0-M4"
+  val playJsonVersion = "2.6.0-M6"
   val playJson = "com.typesafe.play" %% "play-json" % playJsonVersion
 
-  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "1.7.22")
+  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "1.7.25")
 
-  val slf4jtest = Seq("uk.org.lidalia" % "slf4j-test" % "1.1.0")
+  val slf4jtest = Seq("uk.org.lidalia" % "slf4j-test" % "1.2.0")
 
   val javaxInject = Seq("javax.inject" % "javax.inject" % "1")
 
-  val sslConfigVersion = "0.2.1"
+  val sslConfigVersion = "0.2.2"
   val sslConfigCore = Seq("com.typesafe" %% "ssl-config-core" % sslConfigVersion)
 
   val scalaXmlVersion = "1.0.6"
@@ -36,14 +36,14 @@ object Dependencies {
   val cachecontrolVersion = "1.1.2"
   val cachecontrol = Seq("com.typesafe.play" %% "cachecontrol" % cachecontrolVersion)
 
-  val asyncHttpClientVersion = "2.0.30"
+  val asyncHttpClientVersion = "2.0.31"
   val asyncHttpClient = Seq(
     "org.asynchttpclient" % "async-http-client" % asyncHttpClientVersion
   )
 
-  val akkaVersion = "2.4.14"
+  val akkaVersion = "2.5.0"
   val akka = Seq("com.typesafe.akka" %% "akka-stream" % akkaVersion)
-  val akkaHttp = Seq("com.typesafe.akka" %% "akka-http" % "10.0.1")
+  val akkaHttp = Seq("com.typesafe.akka" %% "akka-http" % "10.0.5")
 
   val reactiveStreams = Seq("org.reactivestreams" % "reactive-streams" % "1.0.0")
 
@@ -52,7 +52,7 @@ object Dependencies {
   val jcache = Seq("javax.cache" % "cache-api" % "1.0.0")
 
   // https://mvnrepository.com/artifact/com.github.ben-manes.caffeine/jcache
-  val caffeineVersion = "2.3.5"
+  val caffeineVersion = "2.4.0"
   val caffeine = Seq("com.github.ben-manes.caffeine" % "jcache" % caffeineVersion)
 
   val standaloneApiWSDependencies = javaxInject ++
