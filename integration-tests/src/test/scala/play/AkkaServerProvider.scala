ab5a0416315133903b271de8156e9ffc2644b9d5
Marcos Pereira <marcos.silva@gmail.com>
2017-05-22 17:38:17 -0300

    Tests running with a random port and non-sequentially (#121)

---
+++ integration-tests/src/test/scala/play/AkkaServerProvider.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2009-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package play
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import org.specs2.concurrent.ExecutionEnv
+import org.specs2.specification.BeforeAfterAll
+
+import scala.concurrent.duration._
+import scala.concurrent.{ Await, Future }
+
+trait AkkaServerProvider extends BeforeAfterAll {
+
+  /**
+   * @return Routes to be used by the test.
+   */
+  def routes: Route
+
+  /**
+   * The execution context environment.
+   */
+  def executionEnv: ExecutionEnv
+
+  var testServerPort: Int = _
+  val defaultTimeout: FiniteDuration = 5.seconds
+
+  // Create Akka system for thread and streaming management
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+
+  lazy val futureServer: Future[Http.ServerBinding] = {
+    // Using 0 (zero) means that a random free port will be used.
+    // So our tests can run in parallel and won't mess with each other.
+    Http().bindAndHandle(routes, "localhost", 0)
+  }
+
+  override def beforeAll(): Unit = {
+    val portFuture = futureServer.map(_.localAddress.getPort)(executionEnv.executionContext)
+    portFuture.onSuccess {
+      case port => testServerPort = port
+    }(executionEnv.executionContext)
+    Await.ready(portFuture, defaultTimeout)
+  }
+
+  override def afterAll(): Unit = {
+    futureServer.foreach(_.unbind())(executionEnv.executionContext)
+    val terminate = system.terminate()
+    Await.ready(terminate, defaultTimeout)
+  }
+}
