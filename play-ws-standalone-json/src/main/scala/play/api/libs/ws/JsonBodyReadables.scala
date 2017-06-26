4e106fe0a47215f7640ce144e55411dce4e49b78
Will Sargent <wsargent@users.noreply.github.com>
2017-06-26 13:55:22 -0700

    Use Json.parse for bodyreadables (#160)

--- play-ws-standalone-json/src/main/scala/play/api/libs/ws/JsonBodyReadables.scala
+++ play-ws-standalone-json/src/main/scala/play/api/libs/ws/JsonBodyReadables.scala
@@ -21,20 +21,7 @@ trait JsonBodyReadables {
    * }}}
    */
   implicit val readableAsJson: BodyReadable[JsValue] = BodyReadable { response =>
-    val body = response.bodyAsBytes
-    Json.parse(body.decodeString(detectEncoding(body)))
-  }
-
-  // Leverage Jackson's RFC-4627 / 7159 JSON encoding support.
-  private[ws] def detectEncoding(in: akka.util.ByteString): String = {
-    // https://github.com/FasterXML/jackson-core/blob/master/src/main/java/com/fasterxml/jackson/core/json/ByteSourceJsonBootstrapper.java
-    // Also see https://stackoverflow.com/a/38036753 if this is insufficient
-    import com.fasterxml.jackson.core.io.IOContext
-    import com.fasterxml.jackson.core.json.ByteSourceJsonBootstrapper
-    import com.fasterxml.jackson.core.util.BufferRecycler
-    val ctx = new IOContext(new BufferRecycler, null, false)
-    val strapper = new ByteSourceJsonBootstrapper(ctx, in.toArray, 0, 4)
-    strapper.detectEncoding.getJavaName
+    Json.parse(response.bodyAsBytes.toArray)
   }
 }
 
