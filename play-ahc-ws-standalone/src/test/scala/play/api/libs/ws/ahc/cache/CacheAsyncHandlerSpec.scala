02ff9416a196ebf2b015377b646732c6bde10168
Marcos Pereira <marcos.silva@gmail.com>
2017-07-28 16:05:24 -0300

    Fix NPE in response.body when content type lacks charset (#183)
    * Avoid NPE when getting the body from a cacheable response
    
    * Use defined constants from AHC

--- play-ahc-ws-standalone/src/test/scala/play/api/libs/ws/ahc/cache/CacheAsyncHandlerSpec.scala
+++ play-ahc-ws-standalone/src/test/scala/play/api/libs/ws/ahc/cache/CacheAsyncHandlerSpec.scala
@@ -1,8 +1,6 @@
 /*
  * Copyright (C) 2009-2017 Lightbend Inc. <https://www.lightbend.com>
- *
  */
-
 package play.api.libs.ws.ahc.cache
 
 import org.specs2.mutable.Specification
---
+++ play-ahc-ws-standalone/src/test/scala/play/api/libs/ws/ahc/cache/CacheableResponseSpec.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2009-2017 Lightbend Inc. <https://www.lightbend.com>
+ */
+package play.api.libs.ws.ahc.cache
+
+import org.specs2.mutable.Specification
+import play.shaded.ahc.io.netty.handler.codec.http.HttpHeaders.Names._
+
+class CacheableResponseSpec extends Specification {
+
+  "CacheableResponse" should {
+
+    "get body" in {
+
+      "when it is text/plain" in {
+        val response = CacheableResponse(200, "https://playframework.com/", "PlayFramework Homepage").withHeaders(CONTENT_TYPE -> "text/plain")
+        response.getResponseBody must beEqualTo("PlayFramework Homepage")
+        response.getContentType must beEqualTo("text/plain")
+      }
+
+      "when it is application/json" in {
+        val response = CacheableResponse(200, "https://playframework.com/", """{ "a": "b" }""").withHeaders("Content-Type" -> "application/json")
+        response.getResponseBody must beEqualTo("""{ "a": "b" }""")
+        response.getContentType must beEqualTo("application/json")
+      }
+
+      "when it is application/json; charset=utf-8" in {
+        val response = CacheableResponse(200, "https://playframework.com/", """{ "a": "b" }""").withHeaders("Content-Type" -> "application/json; charset=utf-8")
+        response.getResponseBody must beEqualTo("""{ "a": "b" }""")
+        response.getContentType must beEqualTo("application/json; charset=utf-8")
+      }
+    }
+  }
+}
