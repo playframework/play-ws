1673b4390903ec4dd84c05b16a1e758531d6c323
Martynas Mickevičius <self@2m.lt>
2017-12-18 18:44:23 +0700

    Refactor Scala API RequestFilter tests

--- integration-tests/src/test/scala/play/api/libs/ws/WSRequestFilterSpec.scala
+++ integration-tests/src/test/scala/play/api/libs/ws/WSRequestFilterSpec.scala
@@ -61,94 +61,102 @@ trait WSRequestFilterSpec extends Specification with AkkaServerProvider with Aft
     }
 
     "execute with adhoc request filter" in {
-      withClient() { client =>
-        client.url(s"http://localhost:$testServerPort").withRequestFilter(WSRequestFilter { e =>
-          WSRequestExecutor(r => e.apply(r.withQueryStringParameters("key" -> "some string")))
-        }).get().map { response =>
-          response.body[String] must contain("some string")
-        }.await(retries = 0, timeout = defaultTimeout)
+      withClient() {
+        _.url(s"http://localhost:$testServerPort")
+          .withRequestFilter(WSRequestFilter { e =>
+            WSRequestExecutor(r => e.apply(r.withQueryStringParameters("key" -> "some string")))
+          })
+          .get()
+          .map(_.body[String] must contain("some string"))
+          .awaitFor(defaultTimeout)
       }
     }
 
     "stream with adhoc request filter" in {
-      withClient() { client =>
-        client.url(s"http://localhost:$testServerPort").withRequestFilter(WSRequestFilter { e =>
-          WSRequestExecutor(r => e.apply(r.withQueryStringParameters("key" -> "some string")))
-        }).withMethod("GET").stream().map { response =>
-          response.body[String] must contain("some string")
-        }.await(retries = 0, timeout = defaultTimeout)
+      withClient() {
+        _.url(s"http://localhost:$testServerPort")
+          .withRequestFilter(WSRequestFilter { e =>
+            WSRequestExecutor(r => e.apply(r.withQueryStringParameters("key" -> "some string")))
+          })
+          .withMethod("GET")
+          .stream()
+          .map(_.body[String] must contain("some string"))
+          .awaitFor(defaultTimeout)
       }
     }
 
     "execute with one request filter" in {
-      withClient() { client =>
-        val callList = scala.collection.mutable.ArrayBuffer[Int]()
-        client.url(s"http://localhost:$testServerPort")
+      val callList = scala.collection.mutable.ArrayBuffer[Int]()
+      withClient() {
+        _.url(s"http://localhost:$testServerPort")
           .withRequestFilter(new CallbackRequestFilter(callList, 1))
-          .get().map { _ =>
-            callList must contain(1)
-          }.await(retries = 0, timeout = defaultTimeout)
+          .get()
+          .map(_ => callList must contain(1))
+          .awaitFor(defaultTimeout)
       }
     }
 
     "stream with one request filter" in {
-      withClient() { client =>
-        val callList = scala.collection.mutable.ArrayBuffer[Int]()
-        client.url(s"http://localhost:$testServerPort")
+      val callList = scala.collection.mutable.ArrayBuffer[Int]()
+      withClient() {
+        _.url(s"http://localhost:$testServerPort")
           .withRequestFilter(new CallbackRequestFilter(callList, 1))
-          .withMethod("GET").stream().map { _ =>
-            callList must contain(1)
-          }.await(retries = 0, timeout = defaultTimeout)
+          .withMethod("GET")
+          .stream()
+          .map(_ => callList must contain(1))
+          .awaitFor(defaultTimeout)
       }
     }
 
     "execute with three request filters" in {
-      withClient() { client =>
-        val callList = scala.collection.mutable.ArrayBuffer[Int]()
-        client.url(s"http://localhost:$testServerPort")
+      val callList = scala.collection.mutable.ArrayBuffer[Int]()
+      withClient() {
+        _.url(s"http://localhost:$testServerPort")
           .withRequestFilter(new CallbackRequestFilter(callList, 1))
           .withRequestFilter(new CallbackRequestFilter(callList, 2))
           .withRequestFilter(new CallbackRequestFilter(callList, 3))
-          .get().map { _ =>
-            callList must containTheSameElementsAs(Seq(1, 2, 3))
-          }.await(retries = 0, timeout = defaultTimeout)
+          .get()
+          .map(_ => callList must containTheSameElementsAs(Seq(1, 2, 3)))
+          .awaitFor(defaultTimeout)
       }
     }
 
     "stream with three request filters" in {
-      withClient() { client =>
-        val callList = scala.collection.mutable.ArrayBuffer[Int]()
-        client.url(s"http://localhost:$testServerPort")
+      val callList = scala.collection.mutable.ArrayBuffer[Int]()
+      withClient() {
+        _.url(s"http://localhost:$testServerPort")
           .withRequestFilter(new CallbackRequestFilter(callList, 1))
           .withRequestFilter(new CallbackRequestFilter(callList, 2))
           .withRequestFilter(new CallbackRequestFilter(callList, 3))
-          .withMethod("GET").stream().map { _ =>
-            callList must containTheSameElementsAs(Seq(1, 2, 3))
-          }.await(retries = 0, timeout = defaultTimeout)
+          .withMethod("GET")
+          .stream()
+          .map(_ => callList must containTheSameElementsAs(Seq(1, 2, 3)))
+          .awaitFor(defaultTimeout)
       }
     }
 
     "allow filters to modify the executing request" in {
-      withClient() { client =>
-        val appendedHeader = "X-Request-Id"
-        val appendedHeaderValue = "someid"
-        client.url(s"http://localhost:$testServerPort")
+      val appendedHeader = "X-Request-Id"
+      val appendedHeaderValue = "someid"
+      withClient() {
+        _.url(s"http://localhost:$testServerPort")
           .withRequestFilter(new HeaderAppendingFilter(appendedHeader, appendedHeaderValue))
-          .get().map { response ⇒
-            response.headers("X-Request-Id").head must be_==("someid")
-          }.await(retries = 0, timeout = defaultTimeout)
+          .get()
+          .map(_.headers("X-Request-Id").head must be_==("someid"))
+          .awaitFor(defaultTimeout)
       }
     }
 
     "allow filters to modify the streaming request" in {
-      withClient() { client =>
-        val appendedHeader = "X-Request-Id"
-        val appendedHeaderValue = "someid"
-        client.url(s"http://localhost:$testServerPort")
+      val appendedHeader = "X-Request-Id"
+      val appendedHeaderValue = "someid"
+      withClient() {
+        _.url(s"http://localhost:$testServerPort")
           .withRequestFilter(new HeaderAppendingFilter(appendedHeader, appendedHeaderValue))
-          .withMethod("GET").stream().map { response ⇒
-            response.headers("X-Request-Id").head must be_==("someid")
-          }.await(retries = 0, timeout = defaultTimeout)
+          .withMethod("GET")
+          .stream()
+          .map(_.headers("X-Request-Id").head must be_==("someid"))
+          .awaitFor(defaultTimeout)
       }
     }
   }
