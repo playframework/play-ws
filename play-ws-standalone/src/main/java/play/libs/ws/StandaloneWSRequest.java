3c7aaa8f2a3a4134a5034f3ad06ee293a220fec1
Marcos Pereira <marcos.silva@gmail.com>
2019-04-01 04:21:30 -0400

    Fix MiMa configuration (#314)
    * Fix mima configuration
    
    1. Enable it in all relevant projects. It was missing from play-ahc-ws-standalone
    2. Check against all patch versions in a minor version since we don't follow semver
    3. Add the correct filters based on the most recent changes in #313
    
    * Clarify explanation of withCookies/setCookies methods
    
    * Build using the same version declared in build.sbt

--- play-ws-standalone/src/main/java/play/libs/ws/StandaloneWSRequest.java
+++ play-ws-standalone/src/main/java/play/libs/ws/StandaloneWSRequest.java
@@ -229,8 +229,9 @@ public interface StandaloneWSRequest {
     StandaloneWSRequest addCookies(WSCookie... cookies);
 
     /**
-     * Set the request cookies. This discard the existing ones for this request, but when `play.ws.ahc.useCookieStore`
-     * is true, there is a cookie store that is global and keeps cookies between requests.
+     * Returns this request with the given cookies, discarding the existing ones. In general, cookies set on a request
+     * are used only on that request and then discarded. But when `play.ws.ahc.useCookieStore` is enabled, there will be a
+     * global cookie store that keeps cookies between requests.
      *
      * @param cookies the cookies to be used.
      * @return the modified WSRequest.
