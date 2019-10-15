54be4b7123985f80c8e836a70e3ddd567341229c
Marcos Pereira <marcos.silva@gmail.com>
2019-10-15 11:49:06 -0400

    Update Async-Http-Client to version 2.0.40

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
   val cachecontrolVersion = "1.1.4"
   val cachecontrol = Seq("com.typesafe.play" %% "cachecontrol" % cachecontrolVersion)
 
-  val asyncHttpClientVersion = "2.0.39"
+  val asyncHttpClientVersion = "2.0.40"
   val asyncHttpClient = Seq("org.asynchttpclient" % "async-http-client" % asyncHttpClientVersion)
 
   val akkaVersion = "2.5.17"
