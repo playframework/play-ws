7c8e3c886d7da9cbf3f6680038a08355e2e02880
Marcos Pereira <marcos.silva@gmail.com>
2019-11-19 17:31:30 -0500

    Multiple updates to stable versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,11 +6,11 @@ import sbt._
 object Dependencies {
   val logback = Seq("ch.qos.logback" % "logback-core" % "1.2.3")
 
-  val assertj = Seq("org.assertj" % "assertj-core" % "3.13.2")
+  val assertj = Seq("org.assertj" % "assertj-core" % "3.14.0")
 
   val awaitility = Seq("org.awaitility" % "awaitility" % "4.0.1")
 
-  val specsVersion = "4.8.0"
+  val specsVersion = "4.8.1"
   val specsBuild = Seq(
     "specs2-core",
     "specs2-junit",
@@ -23,14 +23,14 @@ object Dependencies {
 
   val scalaJava8Compat = Seq("org.scala-lang.modules" %% "scala-java8-compat" % "0.9.0")
 
-  val playJsonVersion = "2.8.0-M7"
+  val playJsonVersion = "2.8.0"
   val playJson = Seq("com.typesafe.play" %% "play-json" % playJsonVersion)
 
-  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "1.7.28")
+  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "1.7.29")
 
   val javaxInject = Seq("javax.inject" % "javax.inject" % "1")
 
-  val sslConfigVersion = "0.4.0"
+  val sslConfigVersion = "0.4.1"
   val sslConfigCore = Seq("com.typesafe" %% "ssl-config-core" % sslConfigVersion)
 
   val scalaXmlVersion = "1.2.0"
@@ -39,13 +39,13 @@ object Dependencies {
   val signpostVersion = "1.2.1.2"
   val oauth = Seq("oauth.signpost" % "signpost-core" % signpostVersion)
 
-  val cachecontrolVersion = "2.0.0-M2"
+  val cachecontrolVersion = "2.0.0"
   val cachecontrol = Seq("com.typesafe.play" %% "cachecontrol" % cachecontrolVersion)
 
   val asyncHttpClientVersion = "2.10.4"
   val asyncHttpClient = Seq("org.asynchttpclient" % "async-http-client" % asyncHttpClientVersion)
 
-  val akkaVersion = "2.6.0-RC2"
+  val akkaVersion = "2.6.0"
   val akkaStreams = Seq("com.typesafe.akka" %% "akka-stream" % akkaVersion)
   val akkaHttp = Seq("com.typesafe.akka" %% "akka-http" % "10.1.10")
 
