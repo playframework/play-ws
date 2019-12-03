3957a75d4748b64954b22ecf9707a6f871182cca
Scala Steward <me@scala-steward.org>
2019-12-03 02:57:23 +0100

    Update akka-http to 10.1.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
 
   val akkaVersion = "2.6.0"
   val akkaStreams = Seq("com.typesafe.akka" %% "akka-stream" % akkaVersion)
-  val akkaHttp = Seq("com.typesafe.akka" %% "akka-http" % "10.1.10")
+  val akkaHttp = Seq("com.typesafe.akka" %% "akka-http" % "10.1.11")
 
   val reactiveStreams = Seq("org.reactivestreams" % "reactive-streams" % "1.0.3")
 
