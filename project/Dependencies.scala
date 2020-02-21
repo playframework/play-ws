a112369c68e5b2b87e2554aa96e61f3d7ce2c193
Scala Steward <me@scala-steward.org>
2020-02-21 17:15:44 +0100

    Update ssl-config-core to 0.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
 
   val javaxInject = Seq("javax.inject" % "javax.inject" % "1")
 
-  val sslConfigVersion = "0.4.1"
+  val sslConfigVersion = "0.4.2"
   val sslConfigCore    = Seq("com.typesafe" %% "ssl-config-core" % sslConfigVersion)
 
   val scalaXmlVersion = "1.2.0"
