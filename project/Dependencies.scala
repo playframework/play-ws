789d3ad19c71abeb2f3dc1100358b82b9a56c5ab
Scala Steward <me@scala-steward.org>
2020-09-06 22:46:49 +0200

    assertj-core 3.17.2 (was 3.16.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
   val logback = Seq("ch.qos.logback" % "logback-core" % "1.2.3")
 
-  val assertj = Seq("org.assertj" % "assertj-core" % "3.16.1")
+  val assertj = Seq("org.assertj" % "assertj-core" % "3.17.2")
 
   val awaitility = Seq("org.awaitility" % "awaitility" % "4.0.3")
 
