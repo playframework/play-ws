53d89bbc831da344503157517cbe10a0514fce8a
Scala Steward <me@scala-steward.org>
2022-01-30 16:58:12 +0100

    slf4j-api 1.7.35 (was 1.7.32)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
 
   val playJson = Seq("com.typesafe.play" %% "play-json" % "2.10.0-RC5")
 
-  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "1.7.32")
+  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "1.7.35")
 
   val javaxInject = Seq("javax.inject" % "javax.inject" % "1")
 
