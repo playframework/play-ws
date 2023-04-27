21d74552624c3da0190e3d9f30030b87b3c3c224
Matthias Kurz <m.kurz@irregular.at>
2023-04-27 21:49:11 +0200

    Scala 3.3.0-RC5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   // Should be sync with GA (.github/workflows/build-test.yml)
   val scala213 = "2.13.10"
-  val scala3   = "3.3.0-RC4"
+  val scala3   = "3.3.0-RC5"
 
   val logback = Seq("ch.qos.logback" % "logback-core" % "1.4.7")
 
