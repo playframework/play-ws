b4534d3d82ff7ba320d5e1f16360496b45c74921
Scala Steward <m.kurz@irregular.at>
2024-06-27 16:58:46 +0200

    cachecontrol 3.0.1 (was 3.0.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
 
   val oauth = Seq("oauth.signpost" % "signpost-core" % "2.1.1")
 
-  val cachecontrol = Seq("org.playframework" %% "cachecontrol" % "3.0.0")
+  val cachecontrol = Seq("org.playframework" %% "cachecontrol" % "3.0.1")
 
   val asyncHttpClient = Seq("org.asynchttpclient" % "async-http-client" % "2.12.3")
 
