4abd348cb55d5a8de62cc0993fdbf447f80ebb97
Scala Steward <scala_steward@virtuslab.com>
2025-02-04 04:22:20 +0000

    [3.0.x] logback-core 1.5.16 (was 1.5.15)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val scala213 = "2.13.15"
   val scala3   = "3.3.4"
 
-  val logback = Seq("ch.qos.logback" % "logback-core" % "1.5.15")
+  val logback = Seq("ch.qos.logback" % "logback-core" % "1.5.16")
 
   val assertj = Seq("org.assertj" % "assertj-core" % "3.27.1")
 
