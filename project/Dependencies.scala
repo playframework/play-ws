6159807c605a16c066a1d8e3f1627e65fea3b576
Scala Steward <scala_steward@virtuslab.com>
2025-03-08 01:11:28 +0000

    [2.2.x] slf4j-api 2.0.17 (was 2.0.16)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
 
   val playJson = Seq("com.typesafe.play" %% "play-json" % "2.10.6")
 
-  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "2.0.16")
+  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "2.0.17")
 
   val javaxInject = Seq("javax.inject" % "javax.inject" % "1")
 
