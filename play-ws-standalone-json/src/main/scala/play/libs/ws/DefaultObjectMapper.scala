fc5faf320156b4ff8aae987c89c72088a84f72fb
Matthias Kurz <m.kurz@irregular.at>
2025-11-03 12:19:49 +0100

    DefaultObjectMapper is a Scala object now so we can access private[play] methods

--- play-ws-standalone-json/src/main/scala/play/libs/ws/DefaultObjectMapper.scala
+++ play-ws-standalone-json/src/main/scala/play/libs/ws/DefaultObjectMapper.scala
@@ -2,10 +2,10 @@
  * Copyright (C) from 2022 The Play Framework Contributors <https://github.com/playframework>, 2011-2021 Lightbend Inc. <https://www.lightbend.com>
  */
 
-package play.api.libs.json
+package play.libs.ws
 
 import com.fasterxml.jackson.databind.ObjectMapper
 
-object BadCitizen {
-  def mapper(): ObjectMapper = play.api.libs.json.jackson.JacksonJson.get.mapper
+object DefaultObjectMapper {
+  def instance(): ObjectMapper = play.api.libs.json.jackson.JacksonJson.get.mapper
 }
