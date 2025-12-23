2c29c6b9d9a4c28cc2a3c02193800bcd80fd68a3
Scala Steward <scala_steward@virtuslab.com>
2025-12-23 01:20:59 +0000

    [2.2.x] play-netty-server 2.9.10 (was 2.9.9)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -59,7 +59,7 @@ object Dependencies {
   val akkaStreams = Seq("com.typesafe.akka" %% "akka-stream" % akkaVersion)
 
   val backendServerTestDependencies = Seq(
-    "com.typesafe.play" %% "play-netty-server" % "2.9.9",
+    "com.typesafe.play" %% "play-netty-server" % "2.9.10",
     // Following dependencies are pulled in by play-netty-server, we just make sure
     // now that we use the same akka version here like akka-stream above.
     // This is because when upgrading the akka version in Play and play-ws here we usually release
