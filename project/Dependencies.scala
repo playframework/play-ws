59443ad4d61b56449b4c93b8b25d5294ad01bf88
Matthias Kurz <m.kurz@irregular.at>
2026-05-13 16:09:03 +0200

    slf4j-api 2.0.18 (was 2.0.17)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
 
   val playJson = Seq("org.playframework" %% "play-json" % "3.1.0-M10")
 
-  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "2.0.17")
+  val slf4jApi = Seq("org.slf4j" % "slf4j-api" % "2.0.18")
 
   val jakartaInject = Seq("jakarta.inject" % "jakarta.inject-api" % "2.0.1")
 
